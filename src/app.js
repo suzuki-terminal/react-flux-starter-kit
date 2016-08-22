9ec027b973bae7ab63e0f0baec1c73a1f0bfae0b
Hiroyuki Suzuki <hiro@hiroyukisuzuki.local>
2016-08-22 15:38:59 +0900

    fix starter kit

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+import './app.css';
+
 class App extends Component {
 
     render() {
         return (
             <div>
-                <h1>React Flux Starter Kit</h1>
+                <h1 className="title">React Flux Starter Kit</h1>
             </div>
         );
     }
